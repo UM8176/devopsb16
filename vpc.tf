938646bc4905eadcac2d76ec5516ea18b0511a07
saran01 <saranreddy01@gmail.com>
2021-10-15 06:56:56 +0530

    added IGW for vpc

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "default" {
+    cidr_block = "$172.19.0.0}"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "Devopsb16git"
+	Owner = "saran"
+	environment = "Development"
+    DeployedBy = "Terraform"
+    }
+}
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+	tags = {
+        Name = "devopsb16git-igw"
+    }
+}
